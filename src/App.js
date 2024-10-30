9cc932857299531ca66bd8826a671488b4d32f44
AliGates915 <hacktech877@gmail.com>
2024-10-30 04:29:58 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@
 import "./index.css";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
-import List from "./pages/list/List";
-import New from "./pages/new/New";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./style/dark.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
-import { AuthContext } from "./context/AuthContext";
+
 import Layout from "../src/layout/Layout";
 
 
@@ -24,17 +22,7 @@ import JournalVoucher from "./pages/services/JournalVoucher";
 function App() {
   const { darkMode } = useContext(DarkModeContext);
 
-  const ProtectedRoute = ({ children }) => {
-    const { user } = useContext(AuthContext);
 
-  
-    // If no user or no authentication cookie, redirect to login
-    if (!user ) {
-      return <Navigate to="/login" />;
-    }
-
-    return children;
-  };
 
   return (
     <div className={darkMode ? "app dark" : "app"}>
@@ -45,9 +33,9 @@ function App() {
             <Route
               index
               element={
-                <ProtectedRoute>
+               
                   <Home />
-                </ProtectedRoute>
+                
               }
             />
             
@@ -55,61 +43,61 @@ function App() {
             <Route
               path="info"
               element={
-                <ProtectedRoute>
+                
                   <Layout>
                     <ComapnyInfo />
                   </Layout>
-                </ProtectedRoute>
+                
               }
             />
             <Route
               path="head"
               element={
-                <ProtectedRoute>
+                
                   <Layout>
                     <Head />
                   </Layout>
-                </ProtectedRoute>
+                
               }
             />
             <Route
               path="sub-head"
               element={
-                <ProtectedRoute>
+                
                   <Layout>
                     <SubHead />
                   </Layout>
-                </ProtectedRoute>
+              
               }
             />
              <Route
               path="account-head"
               element={
-                <ProtectedRoute>
+               
                   <Layout>
                     <AccountHead />
                   </Layout>
-                </ProtectedRoute>
+               
               }
             />
              <Route
               path="item-balance"
               element={
-                <ProtectedRoute>
+                
                   <Layout>
                     <ItemOpening />
                   </Layout>
-                </ProtectedRoute>
+                
               }
             />
              <Route
               path="voucher"
               element={
-                <ProtectedRoute>
+                
                   <Layout>
                     <JournalVoucher />
                   </Layout>
-                </ProtectedRoute>
+               
               }
             />
           
